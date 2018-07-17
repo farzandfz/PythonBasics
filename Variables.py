af7d3107c3669b378620138c8c8c812de8f05f1a
Farzand Khan <farzand.ranchi@gmail.com>
2018-07-17 23:14:22 +0530

    adding for loop file

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#this is a variable
+a=100
+
+#this is a packed variable, we'll have to unpack everything, else python will get mad
+#that's what I know as of now
+x,y=(3,5)
+
+print(x,y)
+print(a)
+print(y)
+
+#variables were supposed to be simple, but this packing unpacking
+#thing is making it comlicated, I'll figure it out later
---
+++ for loop.py
@@ -0,0 +1,9 @@
+import time
+
+startTime = time.time()
+
+for x in range(1,10):
+    print(sum)
+
+
+print("Elapsed time is %s seconds" % (time.time() - startTime))
