cdaea64de48933e88c2f684c32e231a93bba73cd
Farzand Khan <farzand.ranchi@gmail.com>
2018-07-17 22:52:38 +0530

    adding Basic Math file

---
+++ Basic Math.py
@@ -0,0 +1,21 @@
+'''
+here I'm adding functionality to add, subtract, multiply and divide numbers
+
+'''
+
+#x and y are variables
+x=100
+y=10
+
+add= x+y
+sub= x-y
+mult= x*y
+div= x/y
+expo=x**y
+
+print(add)
+print('Addition of x and y equals:',add)
+print('Subtraction of x and y equals:',sub)
+print('Multiplication of x and y equals:',mult)
+print('Division of x and y equals:',div)
+print('Exponent of x raised to y',expo)
