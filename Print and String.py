e24a40c131dc947fd25658089f0fd8a040629651
Farzand Khan <farzand.ranchi@gmail.com>
2018-07-17 22:37:51 +0530

    adding print python file

---
+++ Print and String.py
@@ -0,0 +1,15 @@
+#this is a comment in a single line
+
+''' this is
+how we add
+multi line comment
+in Python
+'''
+
+print('Just printing some useless code in single quotes')
+print("We can also use double quotes, you know its helpful if I'd like to use apostrophe")
+print('but there is also another way around it. I\'ll show you how')
+print('in that line above -\- thing, the backslash is a escape character, it can do a whole lot of cool stuff ')
+print(' \ n - adds a new line\n'
+      ' \ t - adds a tab')
+print("done, OK")
